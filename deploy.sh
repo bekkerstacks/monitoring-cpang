cff1a50295d35c649539116b40333512c3679018
Bekker Stacks <50801771+bekkerstacks@users.noreply.github.com>
2019-06-30 01:42:11 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,11 @@ FEATURE=${BSTACKS_FEATURE:-default}
 if [ $BSTACKS_MODE == "http" ] && [ $BSTACKS_FEATURE == "dashboards" ]
   then
     echo "deploying monitoring stack in http mode with dashboards pre-configured"
-    docker stack deploy -c alt_versions/docker-compose_http_with_dashboards.yml mon4
+    docker stack deploy -c alt_versions/docker-compose_http_with_dashboards.yml mon
+    echo "Stack is available at:"
+    echo "- http:/grafana.${DOMAIN}"
+    echo "- http:/grafana.${DOMAIN}"
+    echo "- http:/grafana.${DOMAIN}"
   else
     echo "deploying monitoring stack in https mode"
     docker stack deploy -c docker-compose.yml mon
